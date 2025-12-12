9e7053c62f5c847776dbdb605509f1100e8f9af0
houssem-elheni <elheni.med.houssem@gmail.com>
2025-12-12 14:48:38 +0100

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// firebase-config.example.js  (COMMIT THIS)
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.REGION.firebasedatabase.app",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
